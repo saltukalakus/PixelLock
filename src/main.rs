90a1351b6cdd0246531c3e1aeba5db845b36a379
saltukalakus <saltukalakus@gmail.com>
2025-05-24 12:19:58 +0100

    initial files

---
+++ src/main.rs
@@ -0,0 +1,115 @@
+use aes::Aes256;
+use cipher::{block_padding::Pkcs7, BlockDecryptMut, BlockEncryptMut, KeyIvInit};
+use cbc::{Decryptor, Encryptor};
+use image::{io::Reader as ImageReader, ImageError, ImageFormat};
+use rand::{rngs::OsRng, RngCore};
+use std::{fs, io::Cursor, path::Path};
+
+type Aes256CbcEnc = Encryptor<Aes256>;
+type Aes256CbcDec = Decryptor<Aes256>;
+
+fn encrypt_image<P: AsRef<Path> + std::fmt::Debug>(
+    input_image_path: P,
+    output_encrypted_path: P,
+    key: &[u8; 32],
+) -> Result<String, ImageError> {
+    let img = ImageReader::open(&input_image_path)?.decode()?;
+    let original_format = input_image_path
+        .as_ref()
+        .extension()
+        .and_then(|s| s.to_str())
+        .unwrap_or("png")
+        .to_string();
+
+    let mut img_byte_array = Cursor::new(Vec::new());
+    img.write_to(&mut img_byte_array, ImageFormat::Png)?;
+    let img_bytes = img_byte_array.into_inner();
+
+    let mut iv_bytes = [0u8; 16];
+    OsRng.fill_bytes(&mut iv_bytes);
+
+    let cipher = Aes256CbcEnc::new(key.into(), &iv_bytes.into());
+    let mut buffer = vec![0u8; img_bytes.len() + 16]; // Allocate buffer with padding space
+    buffer[..img_bytes.len()].copy_from_slice(&img_bytes);
+    let encrypted_data = cipher
+        .encrypt_padded_mut::<Pkcs7>(&mut buffer, img_bytes.len())
+        .unwrap()
+        .to_vec();
+
+    let mut output_bytes = Vec::new();
+    output_bytes.extend_from_slice(&iv_bytes);
+    output_bytes.extend_from_slice(&encrypted_data);
+
+    fs::write(&output_encrypted_path, output_bytes)?;
+    println!("Image encrypted successfully to: {:?}", output_encrypted_path);
+    Ok(original_format)
+}
+
+fn decrypt_image<P: AsRef<Path> + std::fmt::Debug>(
+    input_encrypted_path: P,
+    output_decrypted_path: P,
+    key: &[u8; 32],
+    original_format: &str,
+) -> Result<(), ImageError> {
+    let full_encrypted_data = fs::read(&input_encrypted_path)?;
+    let (iv_bytes, encrypted_data) = full_encrypted_data.split_at(16);
+
+    let cipher = Aes256CbcDec::new(key.into(), iv_bytes.into());
+    let mut encrypted_data_mut = encrypted_data.to_vec();
+    let decrypted_data = cipher.decrypt_padded_mut::<Pkcs7>(&mut encrypted_data_mut).map_err(|_| {
+        ImageError::Decoding(image::error::DecodingError::new(
+            image::error::ImageFormatHint::Unknown,
+            "Failed to decrypt data".to_string(),
+        ))
+    })?;
+
+    let format = ImageFormat::from_extension(original_format).ok_or_else(|| {
+        ImageError::Decoding(image::error::DecodingError::new(
+            image::error::ImageFormatHint::Unknown,
+            format!("Unknown image format: {}", original_format),
+        ))
+    })?;
+
+    let img = ImageReader::new(Cursor::new(decrypted_data))
+        .with_guessed_format()?
+        .decode()?;
+    img.save_with_format(&output_decrypted_path, format)?;
+
+    println!("Image decrypted successfully to: {:?}", output_decrypted_path);
+    Ok(())
+}
+
+fn main() {
+    let mut encryption_key_bytes = [0u8; 32];
+    OsRng.fill_bytes(&mut encryption_key_bytes);
+    println!("Generated AES Key (hex): {}", hex::encode(&encryption_key_bytes));
+
+    let input_img = "my_secret_image.jpeg";
+    let encrypted_img_output = "my_secret_image.enc";
+    let decrypted_img_output = "my_secret_image_decrypted.jpeg";
+
+    if !Path::new(input_img).exists() {
+        eprintln!("'{}' not found. Creating a dummy PNG image for demonstration.", input_img);
+        let dummy_img = image::RgbImage::new(100, 100);
+        if let Err(e) = dummy_img.save(input_img) {
+            eprintln!("Error creating dummy image: {}", e);
+            return;
+        }
+    }
+
+    match encrypt_image(input_img, encrypted_img_output, &encryption_key_bytes) {
+        Ok(original_format) => {
+            if let Err(e) = decrypt_image(
+                encrypted_img_output,
+                decrypted_img_output,
+                &encryption_key_bytes,
+                &original_format,
+            ) {
+                eprintln!("Error decrypting image: {}", e);
+            }
+        }
+        Err(e) => {
+            eprintln!("Error encrypting image: {}", e);
+        }
+    }
+}
